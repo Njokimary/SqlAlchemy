f2e427f05252da2098585f8546990a56c03b490f
Njokimary <marynjoki738@gmail.com>
2023-09-30 10:50:36 +0300

    create classes users and profile and   link with database

---
+++ main.py
@@ -0,0 +1,31 @@
+import sqlalchemy
+
+##create tables
+#db
+    #users
+    #posts
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import session_maker    
+
+db_url = "sqlite:///testing.db"
+engine = create_engine(db_url)
+
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    off_name = Column(String)
+    user_name = Column(String)
+
+class Profile(Base):
+    __tablename__ = 'profiles'
+
+    id = Column(Integer, primary_key=True)
+    off_name = Column(String)
+    user_name = Column(String)
+
+Base.metadata.create_all(engine)
